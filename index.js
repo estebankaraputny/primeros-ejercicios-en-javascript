52ba532bc1e3c0051f8b9cb2119889944fd2fddf
estebankaraputny <estebankaraputny18@gmail.com>
2022-10-14 21:42:24 -0300

    :tada: Feat: javascript introdution

---
+++ index.js
@@ -0,0 +1,17 @@
+const nombre = "Esteban"; //String
+const apellido = "Karaputny"; //String
+let edad = 22; //Number
+let uni = "Universidad Nacional Arturo Jaureche"; //String
+let carrera = "Ingenería en Informática"; //String
+const añoNacimiento = 2000; //Number
+const mesNacimiento = "Marzo"; //String
+const fechaNacimiento = 12; //Number
+let noviazgo = false; //Boolean
+let music = {
+    genero: "WitchHouse",
+    creacion: 2000,
+    origen: "Reino Unido",
+    bandaFav: "Crim3s",
+    canciones: ["Stay Ugly", "DOSE", "HOLES", "New Flesh", "Still Goin"], //Array
+} //Object
+
